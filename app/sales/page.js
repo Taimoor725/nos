fa9d98e31439054e520c7c227b4ee5cd117d347f
Taimoor725 <taimoorscientist725@gmail.com>
2024-06-03 02:45:28 +0500

    Update page.js

--- app/sales/page.js
+++ app/sales/page.js
@@ -1,19 +1,16 @@
 import Navbar from '@/components/Home/Navbar'
 import React from 'react'
 import Filter from '@/components/sales/Filter'
-import {NextUIProvider} from "@nextui-org/react";
 
 function page() {
   // ABC
 
   return (
-<NextUIProvider>
     <div className='w-screen h-screen overflow-x-hidden bg-[#0C0C14] '>
         <Navbar/>
         <Filter/>
 
     </div>
-</NextUIProvider>
   )
 }
 
