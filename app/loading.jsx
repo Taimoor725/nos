957356f8b7d69078de5a00648d15dac061d0d7dc
Taimoor725 <taimoorscientist725@gmail.com>
2024-06-03 19:13:27 +0500

    loader added

--- app/loading.jsx
+++ app/loading.jsx
@@ -1,15 +1,17 @@
-"use client"
-
-import { NextUIProvider, Spinner } from "@nextui-org/react";
+// components/LoadingScreen.js
 import React from "react";
 
 const Loading = () => {
   return (
-    <div className="w-screen h-screen bg-black flex items-center justify-center relative">
-  <div className="w-[10rem] h-[10rem] border-black border-[4px] rounded-full border-t-white animate-spin"></div>
-  <img src="/Home/whiteLogo.png" alt="loading..." className="w-[10%] h-[10%] absolute inset-0 m-auto" />
+    <div className="w-screen h-screen bg-black flex justify-center items-center">
+		<div className="loader">
+	<div></div>
+	<div></div>
+	<div></div>
+	<div></div>
+	<div></div>
 </div>
-
+</div>
   );
 };
 
