9ce274b7aa5bffc8bdd7e580fa92cbc8d77c969a
Taimoor725 <taimoorscientist725@gmail.com>
2024-06-22 00:44:09 +0500

    Update FrontPage.js

--- components/advertizment/FrontPage.js
+++ components/advertizment/FrontPage.js
@@ -3,7 +3,7 @@ import React from 'react'
 function FrontPage() {
   return (
     <>
-    <div className='w-screen h-screen mb-4 block'>
+    <div className='w-screen h-screen mb-4 '>
     <video
             src="/videos/seconf_intro.mp4"
             className="w-full h-full object-cover z-0 overflow-hidden"
