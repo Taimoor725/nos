4b902d36f771039d4833beb470f22859a542d59d
Taimoor725 <taimoorscientist725@gmail.com>
2024-06-22 02:33:44 +0500

    NavBar

--- components/Home/Navbar.jsx
+++ components/Home/Navbar.jsx
@@ -1,16 +1,16 @@
+import Link from 'next/link'
 import React from 'react'
 
 function Navbar() {
   return (
 
-    <div>
-      {/* <div className=''>
-          <div>RACING</div>
-          <div>SPORT CRAS</div>
-          <div>KOLLEKTIONEN</div>
-          <div>ERLEBNISSE</div>
-          <div>ABOUT US</div>
-      </div> */}
+    <div className='text-white rounded-[16px] font-semibold text-lg items-center left-[50%] p-3 w-[60vw] h-[4rem] flex absolute z-50 custom-NavBar translate-x-[-50%] top-3 justify-around'>
+        <div className='hover:scale-110 transition-transform ease-linear'><img src='company_logo.svg ' alt='LOGO' className=' w-8 h-full object-cover '/></div>
+        <div className=''><Link href={'/home/'}>HOME</Link></div>
+        <div className=''><Link href={'/sales/'}>SALES</Link> </div>
+        <div className=''><Link href={'/corrosion/'}>CORROSION</Link></div>
+        <div className=''><Link href={'/about/'}>ABOUT</Link></div>
+        <div className=''><Link href={'/contact/'}>CONTACT</Link></div>
     </div>
 
   )
